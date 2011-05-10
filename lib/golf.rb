ec37d1c154ef9a355d145bd6dac7f779009b1f0e
Marcus Ortiz <mportiz08@gmail.com>
2011-05-09 18:16:29 -0700

    first commit

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,37 @@
 class Golf
+  def self.hole1(array)
+    array.inject(:*)
+  end
+  
+  def self.hole2(str)
+    # TODO
+  end
+  
+  def self.hole3(num)
+    # TODO
+  end
+  
+  def self.hole4(array)
+    # TODO
+  end
+  
+  def self.hole5(array)
+    # TODO
+  end
+  
+  def self.hole6(num)
+    # TODO
+  end
+  
+  def self.hole7(array)
+    # TODO
+  end
+  
+  def self.hole8(num)
+    # TODO
+  end
+  
+  def self.hole9(filename)
+    # TODO
+  end
 end
